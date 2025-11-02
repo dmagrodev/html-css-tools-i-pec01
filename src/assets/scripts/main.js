ee7da39be92295e2ed14aa53762e90d6663cac8f
dmagrodev <dmagrodev@outlook.com>
2025-11-03 00:51:01 +0100

    configuracion fontawesome y main.js

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,3 +3,12 @@
   const kioto = "Kioto";
   console.log(`Hello, ${kioto}!`);
 } )();
+
+//https://docs.fontawesome.com/apis/javascript aqui documentacion
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+
+import { faCoffee, faHome, faMapMarkerAlt,faBars, faMinus } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCoffee, faHome, faMapMarkerAlt,faBars, faMinus);
+
+dom.watch();
