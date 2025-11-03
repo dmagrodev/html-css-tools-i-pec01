db6a87512d3fdbfa0eb58146247aa8d042df4d90
dmagrodev <dmagrodev@outlook.com>
2025-11-03 22:54:32 +0100

    preparacion nav bar, main.js: togglemenu,añadido listener DOM, initAPP; generacion estructura de carpetas y archivos; SCSS: _header, variables, main, functions

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -12,3 +12,44 @@ import { faCoffee, faHome, faMapMarkerAlt,faBars, faMinus } from '@fortawesome/f
 library.add(faCoffee, faHome, faMapMarkerAlt,faBars, faMinus);
 
 dom.watch();
+
+
+// -----------------------------------------------------------------//
+// TOGGLE MENÚ for mobile view
+// -----------------------------------------------------------------//
+const initMobileMenuToggle = () => {
+    const toggleButton = document.querySelector('.main-nav__toggle');
+    const navList = document.querySelector('.main-nav__list');
+    const toggleIcon = toggleButton ? toggleButton.querySelector('.fas') : null; 
+    const OPEN_MOD = 'main-nav__list--is-open';
+
+    if (toggleButton && navList && toggleIcon) {
+        toggleButton.addEventListener('click', () => {
+
+            navList.classList.toggle(OPEN_MOD);
+            
+            const isExpanded = navList.classList.contains(OPEN_MOD);
+            toggleButton.setAttribute('aria-expanded', isExpanded); 
+            
+            if (isExpanded) {
+                toggleIcon.classList.remove('fa-bars');// HAMBURGUESA
+                toggleIcon.classList.add('fa-times'); // X
+            } else {
+                toggleIcon.classList.remove('fa-times');
+                toggleIcon.classList.add('fa-bars'); 
+            }
+        });
+    }
+};
+
+// -----------------------------------------------------------------//
+// INICIALIZACIÓN COMPONENTES
+// -----------------------------------------------------------------//
+const initApp = () => {
+
+    initMobileMenuToggle();
+};
+// -----------------------------------------------------------------//
+// LISTENER General
+// -----------------------------------------------------------------//
+document.addEventListener('DOMContentLoaded', initApp);
